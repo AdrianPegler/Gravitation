5639c4c74b0726b86286da49cd2daf618def5f9f
Unknown <adrian.pegler@mail.de>
2018-07-17 15:09:56 +0200

    introduced ap_malloc and ap_free

--- math_helper.c
+++ math_helper.c
@@ -1,5 +1,8 @@
 #include "math_helper.h"
 
+size_t bytes_alloced = 0;
+size_t max_alloced = 0;
+
 int max(int a, int b){
   return (a < b)?b:a;
 }
@@ -22,4 +25,15 @@ void _swap_d(double *a, double *b){
     tmp = *a;
     *a  = *b;
     *b  = tmp; 
+}
+
+void *ap_malloc(size_t size, size_t align){
+    bytes_alloced += size;
+    max_alloced = bytes_alloced>max_alloced?bytes_alloced:max_alloced;
+    return _mm_malloc(size, align);
+}
+
+void ap_free(void *ptr, size_t size){
+    bytes_alloced -= size;
+    _mm_free(ptr);
 }
--- math_helper.h
+++ math_helper.h
@@ -1,9 +1,14 @@
 #ifndef MATH_HELPER_H
 #define MATH_HELPER_H
 
+#include <immintrin.h>
+
 int max(int a, int b);
 double _dist_ab(double a, double b);
 void _swap_i(int *a, int *b);
 void _swap_d(double *a, double *b);
+
+void *ap_malloc(size_t size, size_t align);
+void ap_free(void *ptr, size_t size);
 
 #endif //MATH_HELPER_H
